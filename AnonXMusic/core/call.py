e031a095ed2e71758d216d668770609d739feb55
Salamqq <163534782+Salamqq@users.noreply.github.com>
2024-08-09 02:09:56 +0300

    Update call.py

--- AnonXMusic/core/call.py
+++ AnonXMusic/core/call.py
@@ -11,11 +11,13 @@ from pytgcalls.exceptions import (
     AlreadyJoinedError,
     NoActiveGroupCall,
 )
-from pytgcalls.types import Update
-from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
+from pytgcalls.types import (
+    MediaStream,
+    AudioQuality,
+    VideoQuality,
+    Update,
+)
 from pytgcalls.types.stream import StreamAudioEnded
-
 import config
 from AnonXMusic import LOGGER, YouTube, app
 from AnonXMusic.misc import db
